c7600975592c882e0c21dfda9c11e97d1c548fbb
JonasLopez <jlopez@stuy.edu>
2018-04-12 06:37:10 -0400

    hw96 pushed

---
+++ UserOfMazeSolver.java
@@ -0,0 +1,27 @@
+/**
+  Test MazeSolver class.
+  
+  Taken from UserOfMaze:
+  Requires command line arguments:
+  o  the name of a file containing a maze.
+  o  the rank and file where an explorer is starting
+  
+  For example,
+      java UserOfMaze mazes/4cell_treasureWest.txt -1 -1  # no explorer
+
+*/
+
+public class UserOfMazeSolver {
+
+	public static void main(String[] commandLine) 
+		throws java.io.FileNotFoundException {
+        	System.out.println();
+
+        	Maze maze = new Maze( commandLine[0]
+                            	, Integer.parseInt( commandLine[1])
+                            	, Integer.parseInt( commandLine[2])
+                            	);
+
+        	System.out.println(MazeSolver.solveMaze(maze));
+       	}
+}
